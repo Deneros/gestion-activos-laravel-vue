560bebe6b991a8333795ddfdb461801115d39240
Deneros <nicolas.velez99@hotmail.com>
2020-09-30 20:37:48 -0500

    nueva tabla

---
+++ database/migrations/2020_10_01_012907_create_estados_descripcion_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstadosDescripcionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estados_descripcion', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_item');
+            $table->foreign('id_item')->references('id')->on('items');
+            $table->string('estado');
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estados_descripcion');
+    }
+}
